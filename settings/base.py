ce8ddd383b60287fd4d126a798da1b0ba18aef24
Yunior Espinal <yunior.dev@gmail.com>
2020-10-26 00:52:16 -0400

    Added models for Realtors and Listings.

--- settings/base.py
+++ settings/base.py
@@ -1,8 +1,9 @@
 """
 Base settings to build other settings file upon.
 """
-import environ
 from pathlib import Path
+
+import environ
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 
