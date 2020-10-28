863216297d418405f20f7364fd4c2db2a1afc282
Yunior Espinal <yunior.dev@gmail.com>
2020-10-28 18:51:27 -0400

    Customized the admin data.

--- apps/listings/admin.py
+++ apps/listings/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'list_date', 'realtor', 'is_published',)
+    list_display_links = ('id', 'title',)
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    list_per_page = 25
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode',)
+
+
+admin.site.register(Listing, ListingAdmin)
--- apps/realtors/admin.py
+++ apps/realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date',)
+    list_display_links = ('id', 'name',)
+    list_per_page = 25
+    search_fields = ('name',)
+
+
+admin.site.register(Realtor, RealtorAdmin)
